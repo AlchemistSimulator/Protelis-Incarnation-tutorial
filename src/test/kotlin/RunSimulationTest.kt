9aaa904531737164214d1e230aeea6602f453667
Danilo Pianini <danilo.pianini@gmail.com>
2021-04-29 18:08:04 +0200

    Update all demos to work with Alchemist 11

--- src/test/kotlin/RunSimulationTest.kt
+++ src/test/kotlin/RunSimulationTest.kt
@@ -2,7 +2,8 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.file.shouldHaveExtension
 import it.unibo.alchemist.core.implementations.Engine
 import it.unibo.alchemist.core.interfaces.Status
-import it.unibo.alchemist.loader.YamlLoader
+import it.unibo.alchemist.loader.LoadAlchemist
+import it.unibo.alchemist.loader.SimulationModel
 import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import java.io.File
 import it.unibo.alchemist.model.implementations.times.DoubleTime
@@ -26,7 +27,7 @@ class RunSimulationTest<T : Any?> : StringSpec() {
         "run simulations not should throw exception" {
             listOfSimulationPaths
                 .asSequence()
-                .map { YamlLoader(it.inputStream()).getDefault<T, Euclidean2DPosition>() }
+                .map { LoadAlchemist.from(it).getDefault<T, Euclidean2DPosition>().environment }
                 .map { Engine(it, DoubleTime(simulationTime)) }
                 .onEach {
                     it.play()
